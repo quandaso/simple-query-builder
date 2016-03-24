f15b12d48cb7282f04d1012ed7b3253667bfa463
Quan Tran <quantm@vietmobi.vn>
2016-03-24 22:43:13 +0700

    Updates

--- console.php
+++ console.php
@@ -1,7 +1,16 @@
 <?php
 define('ROOT_DIR', dirname(__FILE__));
 require ROOT_DIR . '/vendor/autoload.php';
+use \Qtm\QueryBuilder\Queryable;
 
-$db = new Qtm\QueryBuilder\Queryable();
-echo $db->from('users')->select('id', 'email', 'full_name')->getSql();
+$db = new Queryable('bigcoin_rebuild', 'root', 'quantm');
+
+$r = $db->table('user_test')
+    ->where('id', '>', 1)
+    ->update(['username'=> 'fuck']);
+
+echo "\n";
+echo $db->getLastSql();
+echo "\n";
+print_r($r);
 echo "\n";
--- src/Qtm/QueryBuilder/Queryable.php
+++ src/Qtm/QueryBuilder/Queryable.php
@@ -6,10 +6,20 @@ namespace Qtm\QueryBuilder;
 
 class Queryable
 {
-    private $sql;
+    private $_lastSql = null;
+    private $_pdo = null;
+    private $_limit = null;
+    private $_offset = null;
+    private $_order = null;
+    private $_group = null;
+    private $_table = null;
+    private $_stmt = null;
+    private $_orderDirection = 'ASC';
+
     private $fromStates = array();
     private $selectFields = array();
     private $whereStates = array();
+    private $values = array();
 
     private $operators = array(
         '>' => true,
@@ -20,23 +30,95 @@ class Queryable
         '!=' => true
     );
 
+    public function __construct($db = null, $username = null, $password = null)
+    {
+        if ($db instanceof \PDO) {
+            $this->_pdo = $db;
+        } else if (isset ($db, $username, $password)) {
+            $this->_pdo = new \PDO("mysql:host=localhost;dbname=$db;charset=utf8mb4", $username, $password);
+        }
+    }
+
+    /**
+     * @return static
+     */
     public function from()
     {
+        $this->initQuery();
         $this->fromStates = self::flatArgs(func_get_args());
         return $this;
     }
 
+    private function initQuery()
+    {
+        $this->_lastSql = null;
+        $this->_limit = null;
+        $this->_offset = null;
+        $this->_order = null;
+        $this->_group = null;
+        $this->_table = null;
+        $this->_stmt = null;
+        $this->_orderDirection = 'ASC';
+
+        $this->fromStates = array();
+        $this->selectFields = array();
+        $this->whereStates = array();
+        $this->values = array();
+    }
+
+    /**
+     * @return static
+     */
+    public function table($table)
+    {
+        $this->initQuery();
+        $this->_table = $table;
+        return $this;
+    }
+
+    /**
+     * @return $this
+     */
     public function select()
     {
         $this->selectFields = self::flatArgs(func_get_args());
         return $this;
-
     }
 
-    public function where($field, $opt = null, $value = null)
+    /**
+     * @param string $type
+     * @param $field
+     * @param null $opt
+     * @param null $value
+     * @return $this
+     * @throws \Exception
+     */
+    private function buildWhereQuery($type = 'AND', $field, $opt = null, $value = null)
     {
+        if ($field instanceof \Closure) {
+
+            $callback = $field;
+
+            $this->whereStates[] = array(
+                'type' => $type,
+                'query' => $callback
+            );
+
+            return $this;
+        }
+
+        if ($value === null) {
+            $value = $opt;
+            $opt = '=';
+        } else {
+            if (!isset ($this->operators[$opt])) {
+                throw new \Exception('Invalid operator: ' . $opt);
+            }
+
+        }
+
         $this->whereStates[] = array(
-            'type' => 'AND',
+            'type' => $type,
             'field' => $field,
             'operator' => $opt,
             'value' => $value
@@ -45,76 +127,405 @@ class Queryable
         return $this;
     }
 
-    public function orWhere($field, $opt = null, $value = null)
+    public function orWhereNotNull($field)
     {
         $this->whereStates[] = array(
             'type' => 'OR',
             'field' => $field,
-            'operator' => $opt,
-            'value' => $value
+            'operator' => 'IS NOT NULL',
+            'value' => null,
+            'nullQuery' => true
         );
 
         return $this;
     }
 
-    public function getSql($type = 'SELECT')
+    public function orWhereNull($field)
     {
-        return $this->getSelectSql();
+        $this->whereStates[] = array(
+            'type' => 'OR',
+            'field' => $field,
+            'operator' => 'IS NULL',
+            'value' => null,
+            'nullQuery' => true
+        );
+
+        return $this;
     }
 
-    private function getSelectSql()
+    public function whereNotNull($field)
     {
-        $query = array($this->getSelectState(), $this->getFromState());
-        return implode(' ', $query);
+        $this->whereStates[] = array(
+            'type' => 'AND',
+            'field' => $field,
+            'operator' => 'IS NOT NULL',
+            'value' => null,
+            'nullQuery' => true
+        );
+
+        return $this;
     }
 
+    public function whereNull($field)
+    {
+        $this->whereStates[] = array(
+            'type' => 'AND',
+            'field' => $field,
+            'operator' => 'IS NULL',
+            'value' => null,
+            'nullQuery' => true
+        );
+
+        return $this;
+    }
+
+    /**
+     * @param $field
+     * @param null $opt
+     * @param null $value
+     * @return Queryable
+     * @throws \Exception
+     */
+    public function where($field, $opt = null, $value = null)
+    {
+        return $this->buildWhereQuery('AND', $field, $opt, $value);
+    }
+
+    /**
+     * @param $field
+     * @param null $opt
+     * @param null $value
+     * @return Queryable
+     * @throws \Exception
+     */
+    public function orWhere($field, $opt = null, $value = null)
+    {
+        return $this->buildWhereQuery('OR', $field, $opt, $value);
+    }
+
+    /**
+     * @param $limit
+     * @return $this
+     */
+    public function limit($limit)
+    {
+        $this->_limit = (int) $limit;
+        return $this;
+    }
+
+    /**
+     * @param $offset
+     * @return $this
+     */
+    public function offset($offset)
+    {
+        $this->_offset = (int) $offset;
+        return $this;
+    }
+
+    /**
+     * @param $field
+     * @param string $direction
+     * @throws \Exception
+     * @return $this
+     */
+    public function orderBy($field, $direction = 'ASC')
+    {
+        $direction = strtoupper($direction);
+
+        if ($direction !== 'ASC' && $direction !== 'DESC') {
+            throw new \Exception('Invalid order direction');
+        }
+
+        $this->_order = $field;
+        $this->_orderDirection = $direction;
+
+        return $this;
+    }
+
+    /**
+     * @param $group
+     * @return $this
+     */
+    public function groupBy($group)
+    {
+        $this->_group = $group;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    private function getOrderByState()
+    {
+        if ($this->_order !== null) {
+            return "ORDER BY " . self::quoteColumn($this->_order) . ' ' . $this->_orderDirection;
+        }
+
+        return '';
+    }
+
+    /**
+     * @return string
+     */
+    private function getLimitState()
+    {
+        if ($this->_limit !== null && $this->_offset === null) {
+            return "LIMIT " . $this->_limit;
+        }
+
+        if ($this->_limit !== null && $this->_offset !== null) {
+            return "LIMIT " . $this->_offset . ',' . $this->_limit;
+        }
+
+        return '';
+    }
+
+    /**
+     * @return string
+     * @throws \Exception
+     */
+    public function getSelectSql()
+    {
+        if (empty ($this->fromStates)) {
+            throw new \Exception('Missing FROM statement');
+        }
+
+        $query = array (
+            $this->getSelectState(),
+            $this->getFromState(),
+            $this->getWhereState(),
+            $this->getGroupByState(),
+            $this->getOrderByState(),
+            $this->getLimitState(),
+        );
+
+        return trim(implode(' ', array_filter($query)));
+    }
+
+    /**
+     * @param array $data
+     * @return string
+     * @throws \Exception
+     */
+    public function update(array $data)
+    {
+        if (empty ($this->_table)) {
+            throw new \Exception('Table name is not specified');
+        }
+
+        $updateSetStates = array();
+        $this->values = array();
+
+        foreach ($data as $k => $v) {
+            $updateSetStates[] = self::quoteColumn($k) .'=?';
+            $this->values[] = $v;
+        }
+
+        $query = array (
+            'UPDATE ' . self::quoteColumn($this->_table),
+            'SET ' . implode(',', $updateSetStates),
+            $this->getWhereState(),
+        );
+
+        $this->_lastSql = trim(implode(' ', $query));
+        $this->_stmt = $this->_pdo->prepare($this->_lastSql);
+        $this->bindValues();
+        $this->_stmt->execute();
+
+        return $this->_stmt->rowCount();
+    }
+
+    /**
+     * @param $data
+     * @return string
+     * @throws \Exception
+     */
+    public function insert($data)
+    {
+        if (empty ($this->_table)) {
+            throw new \Exception('Table name is not specified');
+        }
+
+        $insertStates = array();
+        $valueStates = array();
+        $this->values = array();
+
+        foreach ($data as $k => $v) {
+            $insertStates[] = self::quoteColumn($k);
+            $valueStates[] = '?';
+            $this->values[] = $v;
+        }
+
+        $query = array (
+            'INSERT INTO ' . self::quoteColumn($this->_table) . '(' . implode(',', $insertStates) .')',
+            'VALUES(' . implode(',', $valueStates) . ')',
+        );
+
+        return trim(implode(' ', $query));
+    }
+
+    /**
+     * @return string
+     */
     private function getSelectState()
     {
         if (empty ($this->selectFields)) {
             return 'SELECT *';
         }
 
-        $selectFields = array_map(function($value) {
-            return '`' . $value .'`';
-        },$this->selectFields);
-        $selectStates[] = 'SELECT ' . implode(',', $selectFields);
+        $selectFields = array_map('self::quoteColumn', $this->selectFields);
 
-        return implode(' ', $selectStates);
+        return  'SELECT ' . implode(',', $selectFields);
     }
 
+    /**
+     * @return string
+     */
     private function getFromState()
     {
-        $fromTables = array_map(function($value) {
-            return '`' . $value .'`';
-        },$this->fromStates);
-
-        $fromStates[] = 'FROM ' . implode(',', $fromTables);
-
-        return implode(' ', $fromStates);
+        $fromTables = array_map('self::quoteColumn', $this->fromStates);
+        return 'FROM ' . implode(',', $fromTables);
     }
 
-    private static function flatArgs($args)
+    /**
+     * @param $hasWhere
+     * @return string
+     */
+    private function getWhereState($hasWhere = true)
     {
-        $result = [];
-        foreach ($args as $arg) {
-            if (is_string($arg)) {
-                $result[] = $arg;
-            } else if (is_array($arg)) {
-                foreach ($arg as $value) {
-                    if (is_string($value)) {
-                        $result[] = $value;
-                    } else if (is_array($value)) {
-                        foreach (self::flatArgs($value) as $v) {
-                            $result[] = $v;
-                        }
+        if (empty ($this->whereStates)) {
+            return '';
+        }
+
+        $whereStates = array();
+
+        foreach ($this->whereStates as $i => $where) {
+            $first = count($whereStates) === 0;
+
+            if (isset ($where['query'])) {
+                $query = $where['query'](new static());
+
+                if (!empty ($query->whereStates)) {
+                    if ($first) {
+                        $whereStates[] = '(' . $query->getWhereState(false) . ')';
+                    } else {
+                        $whereStates[] = $where['type'] . ' (' . $query->getWhereState(false) . ')';
+                    }
+
+                    foreach ($query->values as $v) {
+                        $this->values[] = $v;
                     }
                 }
+
+            } else if (isset ($where['nullQuery'])) {
+                if ($first) {
+                    $whereStates[] = self::quoteColumn($where['field']) . ' ' . $where['operator'];
+                } else {
+                    $whereStates[] = $where['type'] . ' ' . self::quoteColumn($where['field']) . ' ' . $where['operator'];
+                }
+            } else {
+                $this->values[] = $where['value'];
+
+                if ($first) {
+                    $whereStates[] = self::quoteColumn($where['field']) . ' ' . $where['operator'] . ' ?';
+                } else {
+                    $whereStates[] = $where['type'] . ' ' . self::quoteColumn($where['field']) . ' ' . $where['operator'] . ' ?';
+                }
+            }
+        }
+
+        return  $hasWhere ? 'WHERE ' . implode(' ', $whereStates) : implode(' ', $whereStates);
+    }
+
+    /**
+     * @return array
+     */
+    private function getGroupByState()
+    {
+        if (!empty ($this->_group)) {
+            return 'GROUP BY ' . self::quoteColumn($this->_group);
+        }
+
+        return '';
+    }
+
+    /**
+     * @return array
+     * @throws \Exception
+     */
+    public function get()
+    {
+        $this->_lastSql = $this->getSelectSql();
+        $this->_stmt = $this->_pdo->prepare($this->_lastSql);
+        $this->bindValues();
+        $this->_stmt->execute();
+        $results = $this->_stmt->fetchAll(\PDO::FETCH_ASSOC);
+        return $results;
+    }
+
+    /**
+     * @return null
+     */
+    public function getLastSql()
+    {
+        return $this->_lastSql;
+    }
+
+    /**
+     * Binds values
+     */
+    private function bindValues()
+    {
+        foreach ($this->values as $i => $value) {
+            if (is_string($value)) {
+                $this->_stmt->bindValue($i + 1, $value, \PDO::PARAM_STR);
+            } else if (is_int($value)) {
+                $this->_stmt->bindValue($i + 1, $value, \PDO::PARAM_INT);
+            } else if ($value === null) {
+                $this->_stmt->bindValue($i + 1, null, \PDO::PARAM_NULL);
+            } else {
+                $this->_stmt->bindValue($i + 1, $value, \PDO::PARAM_STR);
+            }
+        }
+    }
+
+    /**
+     * @param $args
+     * @return array
+     */
+    private static function flatArgs($args)
+    {
+        $result = array();
+        foreach ($args as $arg) {
+            if (is_array($arg)) {
+                foreach (self::flatArgs($arg) as $v) {
+                    $result[] = $v;
+                }
+            } else {
+                $result[] = $arg;
             }
         }
 
         return $result;
     }
 
+    /**
+     * @param $field
+     * @return string
+     */
+    private static function quoteColumn($field)
+    {
+        if ($field === '*') {
+            return $field;
+        }
 
+        if (strpos($field, '.') !== false) {
+            list($table, $field) = explode('.', $field);
+            return "`".str_replace("`","``",$table)."`." . "`".str_replace("`","``",$field)."`";
+        }
 
+        return "`".str_replace("`","``",$field)."`";
+    }
 }
