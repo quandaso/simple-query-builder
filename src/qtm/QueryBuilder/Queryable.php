57d18dd8a63c896f65c9cb57bd99c9e8eb01a308
Quan Tran <quantm@vietmobi.vn>
2016-03-30 23:42:01 +0700

    Added Queryable test cases

--- src/qtm/QueryBuilder/Queryable.php
+++ src/qtm/QueryBuilder/Queryable.php
@@ -71,6 +71,7 @@ class Queryable
 
             $this->_pdo = new \PDO("mysql:host=$host;port=$port;dbname=$db;charset=utf8mb4", $username, $password);
             $this->_pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+
         }
     }
 
--- tests/Model/User.php
+++ tests/Model/User.php
@@ -6,8 +6,37 @@
  */
 namespace QtmTest\Model;
 use Qtm\Model\SimpleModel as Model;
+use Qtm\QueryBuilder\Queryable;
 
 class User extends Model
 {
     public $table = 'users';
+
+    private static $connection = null;
+
+    public function __construct($id = null, $table = null)
+    {
+        // singleton pdo connection
+        if (empty (self::$connection)) {
+
+            $config = array (
+                'host' => 'localhost',
+                'database' => 'queryable',
+                'username' => 'root',
+                'password' => 'quantm'
+            );
+
+            $db = $config['database'];
+            $host = $config['host'];
+            $username = $config['username'];
+            $port = isset ($config['port']) ? $config['port'] : 3306;
+            $password = isset ($config['password']) ? $config['password'] : '';
+            self::$connection = new \PDO("mysql:host=$host;port=$port;dbname=$db;charset=utf8mb4", $username, $password);
+            self::$connection->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+        }
+
+        $this->_db = new Queryable(self::$connection, static::class);
+        parent::__construct($id, $table);
+
+    }
 }
